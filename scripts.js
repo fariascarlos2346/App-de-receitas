0c0c86bba896da579dbe09d83ad3779b08b07476
fariascarlos2346 <fariascarlos2346@gmail.com>
2024-10-11 10:14:25 -0300

    colocando a receita na tela

--- scripts.js
+++ scripts.js
@@ -3,31 +3,64 @@
 
     [x] Pegar a informação do Input, quando o botão for clicado
     [x] Ir até a API, e trazer as receitas
-    [] Colocar as receitas na Tela
-    [] Saber quando o usúario clicou na receita
-    [] Buscar informações da receita individual na API
+    [x] Colocar as receitas na Tela
+    [x] Saber quando o usúario clicou na receita
+    [x] Buscar informações da receita individual na API
     [] Colocar a receita individual na tela
 */
 
 const form = document.querySelector('.search-form')
 const recipeList = document.querySelector('.recipe-list')
+const recipeDetails = document.querySelector('.recipe-details')
 
-form.addEventListener('submit', function(event){
+form.addEventListener('submit', function (event) {
     event.preventDefault()
     const inputValue = event.target[0].value
 
     searchRecipes(inputValue)
 })
 
-async function searchRecipes(ingredient){
-    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`)
+async function searchRecipes(ingredient) {
+    const response = await fetch(
+        `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`,
+    )
     const data = await response.json()
 
-    console.log(data)
+    //console.log(data)
+    showRecipes(data.meals)
 }
 
-function showRecipes(recipes){
-    recipes.map(element => {
+function showRecipes(recipes) {
+    recipeList.innerHTML = recipes.map(
+        (item) => `
+        <div class="recipe-card" onclick="getRecipesDetails(${item.idMeal})">
+            <img src="${item.strMealThumb}" alt="receita-foto">
+            <h3>${item.strMeal}</h3>
+        </div>
+        `
+    ).join('')
+}
 
-    })
+async function getRecipesDetails(id){
+    const response = await fetch(
+        `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`,
+    )
+    const data = await response.json()
+    const recipe = data.meals[0]
+
+    let ingredients = ''
+
+    for(let i = 1; i <= 20; i++){
+        if(recipe[`strIngredient${i}`]){
+            ingredients += `<li>${recipe[`strIngredient${i}`]} - ${recipe[`strMeasure${i}`]}</li>`
+        } else {
+            break;
+        }
+    }
+
+    recipeDetails.innerHTML = `
+
+        <ul>${ingredients}</ul>
+    
+    `
 }
