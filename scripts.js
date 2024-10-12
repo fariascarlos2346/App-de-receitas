13c5c5c535c6f5c5e5d1bb630dbbbd74c399aa16
fariascarlos2346 <fariascarlos2346@gmail.com>
2024-10-12 12:25:11 -0300

    Javascript para implementar a funcionalidade de busca.

--- scripts.js
+++ scripts.js
@@ -11,7 +11,7 @@
 
 const form = document.querySelector('.search-form')
 const recipeList = document.querySelector('.recipe-list')
-const recipeDetails = document.querySelector('.recipe-details')
+const recipeDetails = document.querySelector('.recipe-delails')
 
 form.addEventListener('submit', function (event) {
     event.preventDefault()
@@ -25,8 +25,6 @@ async function searchRecipes(ingredient) {
         `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`,
     )
     const data = await response.json()
-
-    //console.log(data)
     showRecipes(data.meals)
 }
 
@@ -43,24 +41,34 @@ function showRecipes(recipes) {
 
 async function getRecipesDetails(id){
     const response = await fetch(
-        `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`,
+        `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
     )
     const data = await response.json()
     const recipe = data.meals[0]
 
+    console.log(recipe)
+
     let ingredients = ''
 
     for(let i = 1; i <= 20; i++){
-        if(recipe[`strIngredient${i}`]){
-            ingredients += `<li>${recipe[`strIngredient${i}`]} - ${recipe[`strMeasure${i}`]}</li>`
+        if(recipe[`strMeasure1${i}`]){
+            ingredients += `<li>${recipe[`strIngredient${i}`]} - ${recipe[`strMeasure${i}`]} </li>`
         } else {
             break;
         }
     }
 
     recipeDetails.innerHTML = `
-
+        <h2>${recipe.strMeal}</h2>
+        <img src="${recipe.strMealThumb}" alt=${recipe.strMeal} class="recipe-img">
+        <h3>Categoria: ${recipe.strCategory}</h3>
+        <h3>Origim: ${recipe.strArea}</h3>
+        <h3>Ingredientes:</h3>
         <ul>${ingredients}</ul>
-    
+        <h3>Instruções:</h3>
+        <p>${recipe.strInstructions}</p>
+        <p>${recipe.strTags}</p>
+        <p>Vídeo: <a href="${recipe.strYoutube}" target="_blank">Assista no YouTube</a></p>
+
     `
 }
